94070e2ae1c47b1b8aee6756b3bd97b72c480927
bao <online988988@gmail.com>
2023-03-21 20:46:02 +0800

    1111

---
+++ drink-pos-system-master/drink-pos-system-master/app.js
@@ -0,0 +1,109 @@
+function Drink(name, sugar, ice) {
+  this.name = name
+  this.sugar = sugar
+  this.ice = ice
+}
+
+Drink.prototype.price = function () {
+  switch (this.name) {
+    case 'Black Tea':
+    case 'Oolong Tea':
+    case 'Baozong Tea':
+    case 'Green Tea':
+      return 30
+    case 'Bubble Milk Tea':
+    case 'Lemon Green Tea':
+      return 50
+    case 'Black Tea Latte':
+    case 'Matcha Latte':
+      return 55
+    default:
+      alert('No this drink')
+  }
+}
+
+//建構式
+function AlphaPos() { }
+
+AlphaPos.prototype.getCheckedValue = function (inputName) {
+  let selectedOption = ''
+  document.querySelectorAll(`[name=${inputName}]`).forEach(function (item) {
+    if (item.checked) {
+      selectedOption = item.value
+    }
+  })
+  return selectedOption
+}
+
+AlphaPos.prototype.addDrink = function (drink) {
+  let orderListCard = `
+  <div class="card mb-3">
+    <div class="card-body pt-3 pr-3">
+      <div class="text-right">
+        <span data-alpha-pos="delete-drink">×</span>
+      </div>
+      <h6 class="card-title mb-1">${drink.name}</h6>
+      <div class="card-text">${drink.ice}</div>
+      <div class="card-text">${drink.sugar}</div>
+    </div>
+    <div class="card-footer text-right py-2">
+      <div class="card-text text-muted">$<span data-drink-price>${drink.price()}</span></div>
+    </div>
+  </div>
+  `
+  orderLists.insertAdjacentHTML('afterbegin', orderListCard)
+}
+
+AlphaPos.prototype.deleteDrink = function (target) {
+  target.remove()
+}
+
+AlphaPos.prototype.checkout = function () {
+  let totalAmount = 0
+  document.querySelectorAll('[data-drink-price]').forEach((drink) => {
+    totalAmount += Number(drink.textContent)
+  })
+  return totalAmount
+}
+
+AlphaPos.prototype.clearOrder = function (target) {
+  target.querySelectorAll('.card').forEach((card) => {
+    card.remove()
+  })
+}
+
+//產生實例
+const alphaPos = new AlphaPos()
+
+const addDrinkButton = document.querySelector('[data-alpha-pos="add-drink"]')
+const orderLists = document.querySelector('[data-order-lists]')
+const checkoutButton = document.querySelector('[data-alpha-pos="checkout"]')
+
+//新增飲料
+addDrinkButton.addEventListener('click', () => {
+  const drinkName = alphaPos.getCheckedValue('drink')
+  const ice = alphaPos.getCheckedValue('ice')
+  const sugar = alphaPos.getCheckedValue('sugar')
+  if (!drinkName) {
+    alert('Please choose at least one item.')
+    return
+  }
+  const drink = new Drink(drinkName, sugar, ice)
+  alphaPos.addDrink(drink)
+})
+
+//刪除訂單
+orderLists.addEventListener('click', (e) => {
+  let isDeleteButton = e.target.matches('[data-alpha-pos="delete-drink"]')
+  if (!isDeleteButton) {
+    return
+  }
+  alphaPos.deleteDrink(e.target.parentElement.parentElement.parentElement)
+})
+
+//結帳總金額
+checkoutButton.addEventListener('click', (e) => {
+  alert(`Total amount of drinks: $${alphaPos.checkout()}`)
+  alphaPos.clearOrder(orderLists)
+})
+
